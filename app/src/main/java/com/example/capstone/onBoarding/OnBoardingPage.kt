91da28af2d5e8716504846f9fbf23458572915d6
Dzakk1 <muhadzdzakyzaidan@gmail.com>
2023-11-28 14:49:29 +0700

    menambahkan tampilan onboarding-page

---
+++ app/src/main/java/com/example/capstone/onBoarding/OnBoardingPage.kt
@@ -0,0 +1,12 @@
+package com.example.capstone.onBoarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstone.R
+
+class OnBoardingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_page)
+    }
+}
